5c6cd36c1ba3c4134a86882c264801adf7cf21b5
aplikacjeweterynaryjne-dev <aplikacje.weterynaryjne@gmail.com>
2025-10-24 21:24:20 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // 🔒 Nazwa pamięci podręcznej (zmieniona na v2, aby wymusić aktualizację)
-const CACHE_NAME = 'karta-leczenia-cache-v64';
+const CACHE_NAME = 'karta-leczenia-cache-v65';
 
 // 📦 Lista plików do zapamiętania offline (tzw. App Shell)
 const urlsToCache = [
